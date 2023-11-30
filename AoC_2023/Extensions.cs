72633728c1f44cee3c5f5ae376a922b41de09e3d
tolltech <alexandrovpe@gmail.com>
2023-11-30 16:20:42 +0400

    dummy

---
+++ AoC_2023/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace AoC_2023
+{
+    public static class Extensions
+    {
+        public static int ToIntFromBin(this string src)
+        {
+            return Convert.ToInt32(src, 2);
+        }
+
+        public static long ToLongFromBin(this string src)
+        {
+            return Convert.ToInt64(src, 2);
+        }
+
+        private static readonly Dictionary<char, string> bin = new Dictionary<char, string>
+        {
+            {'0', "0000"},
+            {'1', "0001"},
+            {'2', "0010"},
+            {'3', "0011"},
+            {'4', "0100"},
+            {'5', "0101"},
+            {'6', "0110"},
+            {'7', "0111"},
+            {'8', "1000"},
+            {'9', "1001"},
+            {'A', "1010"},
+            {'B', "1011"},
+            {'C', "1100"},
+            {'D', "1101"},
+            {'E', "1110"},
+            {'F', "1111"},
+        };
+
+        public static string ToBinFromHex(this string src)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in src)
+            {
+                sb.Append(bin[c]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> ts)
+        {
+            return new HashSet<T>(ts);
+        }
+        
+        public static TValue SafeGet<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue val = default)
+        {
+            return dict.TryGetValue(key, out var v) ? v : val;
+        }
+    }
+}
---
+++ AoC_2023/Task01_2.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2023
+{
+    [TestFixture]
+    public class Task01_2
+    {
+        [Test]
+        [TestCase(
+            @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000",
+            45000)]
+        [TestCase(
+            @"Task01.txt"
+            ,
+            200116)]
+        public void Task(string input, int expected)
+        {
+            input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+            var max = input.Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(new[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+                .Select(x => x.Sum())
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum();
+
+            max.Should().Be(expected);
+        }
+    }
+}

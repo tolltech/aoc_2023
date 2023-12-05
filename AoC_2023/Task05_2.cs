a44437eb7632b974f1cdbd3a6aed8f7a7c2be58e
tolltech <alexandrovpe@gmail.com>
2023-12-05 19:46:26 +0400

    slow solution but i have enough time

--- AoC_2023/Task05_2.cs
+++ AoC_2023/Task05_2.cs
@@ -42,24 +42,23 @@ humidity-to-location map:
 60 56 37
 56 93 4",
             46)]
-        [TestCase(@"Task05.txt", 0)]
+        //[TestCase(@"Task05.txt", 84206669)]
         public void Task(string input, long expected)
         {
             input = File.Exists(input) ? File.ReadAllText(input) : input;
 
             var splits = input.SplitEmpty("\r\n\r\n");
 
+            TestContext.Out.WriteLine($"DONE {splits.Length}%");
+
             var inputSeeds = splits[0].SplitEmpty(":")[1].SplitEmpty(" ").Select(long.Parse).ToArray();
-            var seeds = new List<long>();
+            var seeds = new List<(long From, long To)>();
             for (var i = 0; i < inputSeeds.Length; i += 2)
             {
                 var start = inputSeeds[i];
                 var range = inputSeeds[i + 1];
 
-                for (var j = 0; j < range; ++j)
-                {
-                    seeds.Add(start + j);
-                }
+                seeds.Add((start, start + range));
             }
 
             var map = splits.Skip(1)
@@ -73,7 +72,8 @@ humidity-to-location map:
                         {
                             SourceFrom = numbers[1],
                             SourceTo = numbers[1] + numbers[2],
-                            Destination = numbers[0],
+                            DestinationFrom = numbers[0],
+                            DestinationTo = numbers[0] + numbers[2],
                             Range = numbers[2]
                         };
                     }).ToArray();
@@ -82,24 +82,72 @@ humidity-to-location map:
 
             var result = long.MaxValue;
 
-            foreach (var seed in seeds)
+            seeds = Merge(seeds);
+
+            foreach (var mergedSeed in seeds)
             {
-                var currentSeed = seed;
-                foreach (var step in map)
+                for (var seed = mergedSeed.From; seed < mergedSeed.To; seed++)
                 {
-                    var suitableMap = step.FirstOrDefault(x => x.SourceFrom <= currentSeed && x.SourceTo > currentSeed);
-                    if (suitableMap != null)
+                    var progress = (int)((seed - mergedSeed.From) * 100.0 / (mergedSeed.To - mergedSeed.From));
+                    if (seed % 1000000 == 0)
+                        TestContext.Out.WriteLine($"DONE {progress}%");
+
+                    if (!seeds.Any(x => x.From <= seed && x.To > seed)) continue;
+
+                    var currentSeed = seed;
+                    foreach (var step in map)
                     {
-                        currentSeed = currentSeed - suitableMap.SourceFrom + suitableMap.Destination;
+                        var suitableMap = step.FirstOrDefault(x => x.SourceFrom <= currentSeed && x.SourceTo > currentSeed);
+                        if (suitableMap != null)
+                        {
+                            currentSeed = currentSeed - suitableMap.SourceFrom + suitableMap.DestinationFrom;
+                        }
                     }
+
+                    var newResult = currentSeed;
+
+                    if (newResult < result) result = newResult;
                 }
+    
+            }
+            
+            result.Should().Be(expected);
+        }
 
-                var newResult = currentSeed;
+        private List<(long From, long To)> Merge(List<(long From, long To)> seeds)
+        {
+            var sortedSeeds = new Queue<(long From, long To)>(seeds.OrderBy(x => x.From));
 
-                if (newResult < result) result = newResult;
+            var result = new List<(long From, long To)>();
+            result.Add(sortedSeeds.Dequeue());
+
+            while (true)
+            {
+                if (sortedSeeds.Count == 0) break;
+                var newSeed = sortedSeeds.Dequeue();
+                var merged = Merge(result.Last(), newSeed);
+                if (merged.Length == 2)
+                {
+                    result.Add(newSeed);
+                }
+                else
+                {
+                    result.RemoveAt(result.Count - 1);
+                    result.Add(merged.Single());
+                }
             }
 
-            result.Should().Be(expected);
+            return result;
+        }
+
+        private (long From, long To)[] Merge((long From, long To) left, (long From, long To) right)
+        {
+            var sorted = new[] { left, right }.OrderBy(x => x.From).ToArray();
+            left = sorted[0];
+            right = sorted[1];
+
+            if (left.To < right.From) return new[] { left, right };
+            return new[] { (left.From, Math.Max(right.To, left.To)) };
         }
     }
 }

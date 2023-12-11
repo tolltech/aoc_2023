a90d294f9c8936244c3d59e67134816bb957a2e1
tolltech <alexandrovpe@gmail.com>
2023-12-12 01:12:19 +0400

    10_2 no

--- AoC_2023/Task10_2.cs
+++ AoC_2023/Task10_2.cs
@@ -43,7 +43,8 @@ L--J.L7...LJS7F-7L7.
 ....L---J.LJ.LJLJ...",
             8)]
         [TestCase(
-            @"FF7FSF7F7F7F7F7F---7
+            @"....................
+FF7FSF7F7F7F7F7F---7
 L|LJ||||||||||||F--J
 FL-7LJLJ||||||LJL-77
 F--JF--7||LJLJ7F7FJ-
@@ -61,7 +62,7 @@ L7JLJL-JLJLJL--JLJ.L",
 
             var map = input.Replace("O", ".").Replace("I", ".").SplitLines().Select(x => x.ToArray()).ToArray();
             var start = (Row: 0, Column: 0);
-            
+
             for (var i = 0; i < map.Length; ++i)
             for (var j = 0; j < map[i].Length; j++)
             {
@@ -72,13 +73,15 @@ L7JLJL-JLJLJL--JLJ.L",
                 }
             }
 
+            var oldLoop = GetLoop(map, start);
+
             var startItem = ('S', start);
-            
+
             var downStep = MakeStep(start, DownStep);
             var upStep = MakeStep(start, TopStep);
             var leftStep = MakeStep(start, LeftStep);
             var rightStep = MakeStep(start, RightStep);
-            
+
             var stepsS = new List<(int, int)>();
             if (IsConnected(startItem, (map[downStep.Item1][downStep.Item2], downStep))) stepsS.Add(DownStep);
             if (IsConnected(startItem, (map[upStep.Item1][upStep.Item2], upStep))) stepsS.Add(TopStep);
@@ -91,7 +94,7 @@ L7JLJL-JLJLJL--JLJ.L",
                 .Single(x => x.Value.Intersect(stepsS).Count() == 2)
                 .Key;
             map[start.Row][start.Column] = sReplace;
-            
+
             var scaledMap = new (char Item, bool Real)[map.Length * 3][];
             for (var i = 0; i < scaledMap.Length; ++i)
             {
@@ -105,13 +108,15 @@ L7JLJL-JLJLJL--JLJ.L",
                 var newJ = 1 + 3 * j;
                 var c = map[i][j];
                 scaledMap[newI][newJ] = (c, true);
-                
+
                 for (var ii = -1; ii <= 1; ++ii)
                 for (var jj = -1; jj <= 1; ++jj)
                 {
                     if (ii == 0 && jj == 0) continue;
                     scaledMap[newI + ii][newJ + jj] = ('.', false);
                 }
+
+                if (!oldLoop.Contains((i, j))) continue;
 
                 var availableSteps = AvailableSteps[c];
                 foreach (var step in availableSteps)
@@ -126,61 +131,121 @@ L7JLJL-JLJLJL--JLJ.L",
 
             var s = Print(scaledMap);
             //--------------------------------------------------------------
-            var visited = new Dictionary<(int Row, int Column), int>();
-            visited[start] = 0;
+
+            start = (1 + 3 * start.Row, 1 + 3 * start.Column);
+
+            var loop = new HashSet<(int Row, int Column)> { start };
             var queue = new Queue<(int Row, int Column)>();
             queue.Enqueue(start);
             while (queue.Count > 0)
             {
                 var current = queue.Dequeue();
-                var l = visited[current];
-                var connectedNeighbours = Extensions.GetVerticalHorizontalNeighbours<char>(map, current)
-                    .Where(x => !visited.ContainsKey(x.Index))
-                    .Where(x => IsConnected(x, (map[current.Row][current.Column], current)))
+                var connectedNeighbours = Extensions.GetVerticalHorizontalNeighbours(scaledMap, current)
+                    .Where(x => !loop.Contains(x.Index))
+                    .Where(x => IsConnected((x.Item.Item, x.Index),
+                        ((scaledMap[current.Row][current.Column].Item), current)))
                     .ToArray();
 
                 foreach (var neighbour in connectedNeighbours)
                 {
-                    visited[neighbour.Index] = l + 1;
+                    loop.Add(neighbour.Index);
                     queue.Enqueue(neighbour.Index);
                 }
             }
 
-            var loop = visited.Keys.ToHashSet();
-            var processedPoints = new HashSet<(int Row, int Column)>();
+            var startNeighbours = Extensions.GetAllNeighbours(scaledMap, start);
 
-            for (var i = 0; i < map.Length; ++i)
-            for (var j = 0; j < map[i].Length; j++)
+            var result = 0;
+            foreach (var startNeighbour in startNeighbours)
             {
-                if (map[i][j] != '.') continue;
-                if (processedPoints.Contains((i, j))) continue;
+                if (startNeighbour.Item.Item != '.') continue;
 
-                GetArea(map, (i, j));
+                var area = GetArea(scaledMap, startNeighbour.Index, loop);
+                if (!area.Edge)
+                {
+                    result = area.Area.Count(x => scaledMap[x.Row][x.Column].Real);
+                    break;
+                }
             }
 
-
-            var result = visited.Values.Max();
             result.Should().Be(expected);
         }
 
-        private static string Print((char Item, bool Real)[][] valueTuples)
+        private HashSet<(int, int)> GetLoop(char[][] map, (int Row, int Column) start)
+        {
+            var loop = new HashSet<(int Row, int Column)> { start };
+            var queue = new Queue<(int Row, int Column)>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                var connectedNeighbours = Extensions.GetVerticalHorizontalNeighbours(map, current)
+                    .Where(x => !loop.Contains(x.Index))
+                    .Where(x => IsConnected((x.Item, x.Index),
+                        ((map[current.Row][current.Column]), current)))
+                    .ToArray();
+
+                foreach (var neighbour in connectedNeighbours)
+                {
+                    loop.Add(neighbour.Index);
+                    queue.Enqueue(neighbour.Index);
+                }
+            }
+
+            return loop;
+        }
+
+        private static string Print((char Item, bool Real)[][] map, HashSet<(int, int)>? xs = null)
         {
             var sb = new StringBuilder();
-            foreach (var row in valueTuples)
+            for (var i = 0; i < map.Length; i++)
             {
-                foreach (var tuple in row)
+                var row = map[i];
+                for (var index = 0; index < row.Length; index++)
                 {
-                    sb.Append(tuple.Item);
+                    var tuple = row[index];
+                    sb.Append(xs?.Contains((i, index)) ?? false ? tuple.Real ? 'R' : 'X' :
+                        tuple.Real && tuple.Item == '.' ? 'r' : tuple.Item);
                 }
+
                 sb.AppendLine();
             }
 
             return sb.ToString();
         }
-        
-        private (HashSet<(int Row, int Column)> Area, bool Was) GetArea(char[][] map, (int i, int j) point)
+
+        private (HashSet<(int Row, int Column, bool Real, char Item)> Area, bool Edge) GetArea(
+            (char Item, bool Real)[][] map, (int i, int j) point, HashSet<(int Row, int Column)> loop)
         {
-            throw new NotImplementedException();
+            var queue = new Queue<(int i, int j)>();
+            queue.Enqueue(point);
+            var visited = new HashSet<(int, int)>();
+            var edge = false;
+            var other = false;
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (visited.Contains(current)) continue;
+
+                visited.Add(current);
+                var s = Print(map, visited);
+                var neighbours = Extensions.GetVerticalHorizontalNeighbours(map, current).ToArray();
+                if (neighbours.Length != 4)
+                {
+                    edge = true;
+                }
+
+                foreach (var next in neighbours.Where(x => !loop.Contains(x.Index)))
+                {
+                    queue.Enqueue(next.Index);
+                }
+            }
+
+            var s2 = Print(map, visited);
+
+            return (
+                new HashSet<(int Row, int Column, bool Real, char Item)>(visited.Select(x =>
+                    (x.Item1, x.Item2, map[x.Item1][x.Item2].Real, map[x.Item1][x.Item2].Item))), edge);
         }
 
         private bool IsConnected((char Item, (int Row, int Col) Index) one, (char Item, (int Row, int Col) Index) other)

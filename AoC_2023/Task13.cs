c2d8b8f0480f99d2804cac39638a4fbb325b1552
tolltech <alexandrovpe@gmail.com>
2023-12-13 15:51:45 +0400

    13

---
+++ AoC_2023/Task13.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2023
+{
+    [TestFixture]
+    public class Task13
+    {
+        [Test]
+        [TestCase(
+            @"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#",
+            405)]
+        [TestCase(@"Task13.txt", 34772)]
+        public void Task(string input, int expected)
+        {
+            input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+            var result = 0;
+
+            var maps = input.SplitEmpty("\r\n\r\n", "\n\n")
+                .Select(x => x.SplitLines());
+            foreach (var map in maps)
+            {
+                var rows = map.ToArray();
+                var columns = new List<string>();
+
+                for (var i = 0; i < map[0].Length; ++i)
+                {
+                    var column = new string(map.Select(x => x[i]).ToArray());
+                    columns.Add(column);
+                }
+
+                var rowMirror = GetMirror(rows);
+                var columnMirror = GetMirror(columns.ToArray());
+                if (rowMirror != -1) result += 100 * rowMirror;
+                else if (columnMirror != -1) result += columnMirror;
+                else throw new NotImplementedException();
+            }
+
+            result.Should().Be(expected);
+        }
+
+        private int GetMirror(string[] rows)
+        {
+            for (var i = 1; i < rows.Length; ++i)
+            {
+                var left = i - 1;
+                var right = i;
+                if (CheckMirror(left, right, rows)) return i;
+            }
+
+            return -1;
+        }
+
+        private bool CheckMirror(int left, int right, string[] rows)
+        {
+            while (true)
+            {
+                if (rows[left] != rows[right]) return false;
+
+                if (left == 0 || right == rows.Length - 1)
+                {
+                    break;
+                }
+
+                left--;
+                right++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AoC_2023/Task13_2.cs
@@ -0,0 +1,123 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2023
+{
+    [TestFixture]
+    public class Task13_2
+    {
+        [Test]
+        [TestCase(
+            @"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#",
+            400)]
+        [TestCase(@"Task13.txt", 35554)]
+        public void Task(string input, int expected)
+        {
+            input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+            var result = 0;
+
+            var maps = input.SplitEmpty("\r\n\r\n", "\n\n")
+                .Select(x => x.SplitLines().Select(y => y.ToArray()).ToArray());
+            foreach (var map in maps)
+            {
+                var rows = map.Select(x => new string(x)).ToArray();
+                var columns = new List<string>();
+
+                for (var i = 0; i < map[0].Length; ++i)
+                {
+                    var column = new string(map.Select(x => x[i]).ToArray());
+                    columns.Add(column);
+                }
+
+                var oldRowMirror = GetMirror(rows);
+                var oldColumnMirror = GetMirror(columns.ToArray());
+
+                foreach (var mutatedMapTuple in Mutate(map))
+                {
+                    var mutatedMap = mutatedMapTuple.Item1;
+                    var rowMustInclude = mutatedMapTuple.Item2.Item1;
+                    var columnMustInclude = mutatedMapTuple.Item2.Item2;
+                    rows = mutatedMap.Select(x => new string(x)).ToArray();
+                    columns = new List<string>();
+
+                    for (var i = 0; i < mutatedMap[0].Length; ++i)
+                    {
+                        var column = new string(mutatedMap.Select(x => x[i]).ToArray());
+                        columns.Add(column);
+                    }
+
+                    var rowMirror = GetMirror(rows, rowMustInclude);
+                    var columnMirror = GetMirror(columns.ToArray(), columnMustInclude);
+                    if (rowMirror != -1) result += 100 * rowMirror;
+                    else if (columnMirror != -1) result += columnMirror;
+                    else continue;
+
+                    break;
+                }
+            }
+
+            result.Should().Be(expected);
+        }
+
+        private IEnumerable<(char[][], (int, int))> Mutate(char[][] map)
+        {
+            for (var i = 0; i < map.Length; ++i)
+            for (var j = 0; j < map[0].Length; ++j)
+            {
+                var tmp = map[i][j];
+                map[i][j] = tmp == '#' ? '.' : '#';
+                yield return (map.Select(x => x.ToArray()).ToArray(), (i, j));
+                map[i][j] = tmp;
+            }
+        }
+
+        private int GetMirror(string[] rows, int mustInclude = -1)
+        {
+            for (var i = 1; i < rows.Length; ++i)
+            {
+                var left = i - 1;
+                var right = i;
+
+                if (CheckMirror(left, right, rows, mustInclude)) return i;
+            }
+
+            return -1;
+        }
+
+        private bool CheckMirror(int left, int right, string[] rows, int mustInclude)
+        {
+            while (true)
+            {
+                if (rows[left] != rows[right]) return false;
+
+                if (left == 0 || right == rows.Length - 1)
+                {
+                    break;
+                }
+
+                left--;
+                right++;
+            }
+
+            if (mustInclude == -1)
+                return true;
+
+            return mustInclude >= left && mustInclude <= right;
+        }
+    }
+}
